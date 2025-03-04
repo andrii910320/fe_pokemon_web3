5c3bebd2c3987adf85b9761b02d3c817e5e8ef3a
andrii910320 <128429619+andrii910320@users.noreply.github.com>
2025-03-04 13:45:35 +0200

    Update getNonce.js

--- src/api/auth/getNonce.js
+++ src/api/auth/getNonce.js
@@ -1,10 +1,7 @@
 export const fetchNonce = async (address) => {
   const BE_URL = process.env.REACT_APP_BE_URL;
   try {
-    const res = await fetch(`${BE_URL}/auth/nonce/${address}`, {
-      method: "GET",
-      credentials: "include",
-    });
+    const res = await fetch(`${BE_URL}/auth/nonce/${address}`);
     const response = await res.json();
     const { nonce } = response.data;
     return nonce;
